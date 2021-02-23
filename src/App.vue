39e632e8fee2bcdfb91b88cc41c606f974d0d1d4
chenghz98 <2313902108@qq.com>
2021-02-23 20:51:03 +0800

    创建并配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
     <router-view />
-    <h1>黑马头条</h1>
-    <i class="toutiao toutiao-gengduo"></i>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
   </div>
 </template>
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login-container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginIndex',
+
+  data () {
+    return {}
+  },
+
+  methods: {
+    onSubmit () {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
