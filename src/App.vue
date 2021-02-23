100eb124691ef10749a98d9a8407fc3f429c62b2
chenghz98 <2313902108@qq.com>
2021-02-23 15:28:44 +0800

    引入vant和icon图标

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
     <h1>黑马头条</h1>
     <i class="toutiao toutiao-gengduo"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
   </div>
 </template>
 
-<style lang="less">
-</style>
+<style lang="less"></style>
